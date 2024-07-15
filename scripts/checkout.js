3f1aeaa6dfa8b72f6eca3fc0f550725ae4958a15
Artin <artin.tmr@gmail.com>
2024-07-15 19:11:33 +0200

    creating class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSymmary } from "./checkout/paymentSummary.js";
-import "../data/cart-oop.js";
+import "../data/cart-class.js";
 
 renderOrderSummary();
 renderPaymentSymmary();
