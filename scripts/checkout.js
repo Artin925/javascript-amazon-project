0474c58672ea03308ad80eba0bac59914d4e42b4
Artin <artin.tmr@gmail.com>
2024-07-15 18:52:23 +0200

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSymmary } from "./checkout/paymentSummary.js";
+import "../data/cart-oop.js";
 
 renderOrderSummary();
 renderPaymentSymmary();
