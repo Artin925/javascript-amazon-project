4c716dcdf9247d21be1cb2cc0092caab14b67a9b
Artin <artin.tmr@gmail.com>
2024-07-16 22:26:12 +0200

    convert products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSymmary } from "./checkout/paymentSummary.js";
-import "../data/cart-class.js";
+// import "../data/cart-class.js";
 
 renderOrderSummary();
 renderPaymentSymmary();
